e8cd6af81475e359da69333f612e24100f798285
Abbosbek Muydinboev <muydinboevabbosbek@gmail.com>
2020-06-26 03:47:03 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -22,7 +22,7 @@ def monopoly_with_third_tier
       },
       :names => {
         :reading_railroad => {},
-        :pennsylvania_road => {},
+        :pennsylvania_railroad => {},
         :b_and_o_railroad => {},
         :shortline_railroad => {}
       }
