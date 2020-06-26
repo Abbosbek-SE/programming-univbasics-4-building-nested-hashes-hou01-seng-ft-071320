d534af033c1634c0e1fe737cb7ffa3f0ba0adc7a
Abbosbek Muydinboev <muydinboevabbosbek@gmail.com>
2020-06-26 03:29:33 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -8,12 +8,14 @@ def monopoly_with_second_tier
       :rent_in_dollars => {},
       :names => {}
     }
-   }
 end
 
 def monopoly_with_third_tier
- 
-
+ hash[:railroads] = {
+      :pieces => 4,
+      :rent_in_dollars => {},
+      :names => {}
+    }
 end
 
 def monopoly_with_fourth_tier
